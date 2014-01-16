73427f5151b721bfcaec38f9ceb650205d544ba5
junsumida <jun.sumida@mixi.co.jp>
2014-01-16 14:59:09 +0900

    use jp fastest mirrors for yum repos

--- definitions/centos-64/base.sh
+++ definitions/centos-64/base.sh
@@ -10,7 +10,13 @@ enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+yum -y install yum-plugin-fastestmirror
+
+cat > /etc/yum/pluginconf.d/fastestmirror.conf << EOC
+include_only=.jp
+EOC
+
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils rsync
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
